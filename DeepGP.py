12ec593cc4be6fb007ce551da3ffc6520fa547e7
Hoang Cuong <hoangcuong2011@gmail.com>
2017-10-09 19:34:30 +0800

    Add files via upload

--- DeepGP.py
+++ DeepGP.py
@@ -15,6 +15,9 @@ import numpy as np
 import sys
 np.random.seed(42)
 
+
+import copy
+
 # Keras
 from keras.models import Model
 from keras.layers import Input, Dense, Dropout
@@ -62,8 +65,8 @@ def assess_single_layer(model, X_batch, Y_batch):
     return lik, sq_diff 
 
 def assess_sampled(model, X_batch, Y_batch):
-    lik = model.predict_density(X_batch, Y_batch, S)
-    mean_samples, var_samples = model.predict_y(X_batch, 100)
+    lik = model.predict_density(X_batch, Y_batch, 10)
+    mean_samples, var_samples = model.predict_y(X_batch, 1)
     mean = np.average(mean_samples, 0)
     sq_diff = ((mean - Y_batch)**2)
     return lik, sq_diff
@@ -77,14 +80,14 @@ def make_dgp(X, Y, Z, L):
     for l in range(L):
         kernels.append(RBF(input_dim=17, ARD=True))
         
-    mb = 128 if X.shape[0] > 128 else None 
+    mb = 10000 if X.shape[0] > 10000 else None 
     model = DGP(X, Y, Z, kernels, Gaussian(), num_samples=1, minibatch_size=mb)
 
     # same final layer inits we used for the single layer model
-    model.layers[-1].kern.variance = Y_std**2
-    model.likelihood.variance = Y_std*0.1 
-    model.layers[-1].mean_function = Constant(Y_mean)
-    model.layers[-1].mean_function.fixed = True
+    #model.layers[-1].kern.variance = Y_std**2
+    model.likelihood.variance = 0.01 
+    #model.layers[-1].mean_function = Constant(Y_mean)
+    #model.layers[-1].mean_function.fixed = True
     
     # start the inner layers almost deterministically 
     for layer in model.layers[:-1]:
@@ -126,43 +129,48 @@ dataset = np.loadtxt("validdata.txt", delimiter=",")
 
 X_valid = dataset[:,0:17]
 y_valid = dataset[:,17]
-X_train, X_test, X_valid = standardize_data(X_train, X_test, X_valid)
+
+X_train_root = X_train
+
+X_valid_root = X_valid
+
+X_train, X_test, X_valid = standardize_data(copy.deepcopy(X_train_root), X_test, copy.deepcopy(X_valid_root))
+
+
     
 
 X = X_train
 Y = y_train.reshape(-1, 1)	
 
-Xs, Ys = X_test, y_test.reshape(-1, 1)
+Xs1, Ys1 = X_test, y_test.reshape(-1, 1)
+
+dataset = np.loadtxt("testdata.txt.en_de", delimiter=",")
+
+X_test = dataset[:,0:17]
+
+y_test = dataset[:,17]
+
+X_train, X_test, X_valid = standardize_data(X_train_root, X_test, X_valid_root)    
+
+Xs2, Ys2 = X_test, y_test.reshape(-1, 1)
 
 
-Z_100 = kmeans2(X, 1, minit='points')[0]
+
+if(1==1):
+	Z_100 = kmeans2(np.array(X), 100, minit='points')[0]
+
+	m_dgp1 = make_dgp(X, Y, Z_100, 1)
+	m_dgp2 = make_dgp(X, Y, Z_100, 2)
+	m_dgp3 = make_dgp(X, Y, Z_100, 3)
+	m_dgp4 = make_dgp(X, Y, Z_100, 4)
+	m_dgp5 = make_dgp(X, Y, Z_100, 5)
 
 
-m_dgp1 = make_dgp(X, Y, Z_100, 1)
-m_dgp2 = make_dgp(X, Y, Z_100, 2)
-m_dgp3 = make_dgp(X, Y, Z_100, 3)
-m_dgp4 = make_dgp(X, Y, Z_100, 4)
-m_dgp5 = make_dgp(X, Y, Z_100, 5)
-
-
-if (1==1):
-	k = gpflow.kernels.RBF(input_dim=17, ARD=True)
-	Z_100 = kmeans2(np.array(X), 10, minit='points')[0]
-	m = gpflow.sgpr.SGPR(np.array(X), np.array(Y), kern=k, Z=Z_100)
-	m.likelihood.variance = 0.01
-	m.optimize()
-	mean, var = m.predict_y(X_test)
-	rmse_predict = RMSE(y_test.reshape(-1,1), mean)
-	print('Test RMSE:', rmse_predict)
-	#lik, rmse = batch_assess(m, assess_single_layer, Xs, Ys)
-	#print rmse
-
-
-for m, name in zip([m_dgp1, m_dgp2, m_dgp3, m_dgp4, m_dgp5], ['dgp1 (sgp+adam)', 'dgp2', 'dgp3', 'dgp4', 'dgp5']):
-    t = time.time()
-    m.optimize(tf.train.AdamOptimizer(0.01), maxiter=5000)
-    lik, rmse = batch_assess(m, assess_sampled, Xs, Ys)
-    print('Test RMSE:', rmse, name)
-
-
+	for m, name in zip([m_dgp1, m_dgp2, m_dgp3, m_dgp4, m_dgp5], ['dgp1 (sgp+adam)', 'dgp2', 'dgp3', 'dgp4', 'dgp5']):
+	    t = time.time()
+	    m.optimize(tf.train.AdamOptimizer(0.01), maxiter=20000)
+	    lik, rmse = batch_assess(m, assess_sampled, Xs1, Ys1)
+	    print('Test RMSE 1:', rmse, name)
+	    lik, rmse = batch_assess(m, assess_sampled, Xs2, Ys2)
+	    print('Test RMSE 1:', rmse, name)
 
